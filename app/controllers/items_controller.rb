23fa351abd264ec99d6c80747b7bc9112301a932
brandonkwong <brandonjk@gmail.com>
2014-08-29 14:05:37 -0700

    Multiple tags can be created via one form field

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,35 +3,33 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
     @item = Item.new
-
+    
     @tags = Tag.all
     @tag = Tag.new
-
-    # Test Query
-    @tagging = Tagging.where(tag_id: 50)
   end
 
-
-
-
+  # ADD: case insensitive to tags
   def create
     @item = Item.new(item_params)
 
-    # Scope tag method same_tag stores the name from tag_params in @tag,
-    # and performs a query for a tag with that name
-    @tag = Tag.same_tag(tag_params['name'])
-
+    @tags = tag_params['name'].split(' ')
     # logger.debug "\n\n\nDEBUG START \n"
-    # logger.debug @tag.name
+    # logger.debug @tags
     # logger.debug "\nDEBUG END \n\n\n"
 
-    # If a Tag already exists then add it to the Item,
-    # else create a new Tag and add it to the Item
-    if @tag.exists?
-      @item.tags << @tag
-    else
-      unless tag_params['name'].empty?
-        @item.tags.new(tag_params)
+    @tags.each do |t|
+      # Scope tag method same_tag stores the name from tag_params in @same_tag,
+      # and performs a query for a tag with that name
+      @same_tag = Tag.same_tag(t)
+      # If a Tag already exists then add it to the Item
+      if @same_tag.exists?
+        @item.tags << @same_tag
+      else
+        # Create new tags only if there is user input
+        unless @tags.empty?
+          @tag = Tag.create(name: t)
+          Tagging.create(item: @item, tag: @tag)
+        end
       end
     end
 
@@ -42,14 +40,10 @@ class ItemsController < ApplicationController
     end
   end
 
-
-
-
   def destroy
     Item.find(params[:id]).destroy
     redirect_to root_path
   end
-
 
   private
 
