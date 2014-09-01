d0fb8a69ad2d9f004c1faeee9630250b1e7a7bad
brandonkwong <brandonjk@gmail.com>
2014-09-01 16:45:42 -0700

    Updated apiCtrl

--- app/assets/javascripts/controllers/apiCtrl.js
+++ app/assets/javascripts/controllers/apiCtrl.js
@@ -1,8 +1,8 @@
 inlistApp.controller('InListCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.taggings = function() {
-    var data = $http.get('/api/' + $scope.tagQuery).success(function(data, status, headers, config) {
-      $scope.data = data;
+    var data = $http.get('/api/' + $scope.tagQuery).success(function(tag, status, headers, config) {
+      $scope.tag = tag;      
     });
   };
 
