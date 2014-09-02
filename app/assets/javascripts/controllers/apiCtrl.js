7acb5461352f6836d03732e4babbc5ea41a6877a
brandonkwong <brandonjk@gmail.com>
2014-09-01 23:34:49 -0700

    Added ngIf for tag query column

--- app/assets/javascripts/controllers/apiCtrl.js
+++ app/assets/javascripts/controllers/apiCtrl.js
@@ -3,9 +3,6 @@ inlistApp.controller('InListCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.taggings = function() {
     var data = $http.get('/api/' + $scope.tagQuery).success(function(tag, status, headers, config) {
       $scope.tag = tag;
-
-      console.log(tag);
-
     });
   };
 
