c0d132cf1a37e0047da2dc54e8c5912eaa08692f
brandonkwong <brandonjk@gmail.com>
2014-09-03 14:20:47 -0700

    Updated controllers.js

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -7,7 +7,7 @@ inlistApp.controller('InListCtrl', ['$scope', 'Tag', function($scope, Tag) {
   });
 
   // ngInfiniteScroll demo testing
-  $scope.images = [1, 2, 3, 4, 5, 6, 7, 8];
+  $scope.images = [1];
 
   $scope.loadMore = function() {
     var last = $scope.images[$scope.images.length - 1];
