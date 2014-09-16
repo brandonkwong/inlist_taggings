d13d75c637bb63634aaeb4a8f59f4be7906aa300
brandonkwong <brandonjk@gmail.com>
2014-09-16 16:55:51 -0700

    Added locales for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = params[:hl] if params[:hl]
+  end
+
   # Default for Active Model Serializers
   def default_serializer_options  
     { root: false }  
